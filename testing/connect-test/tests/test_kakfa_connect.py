74ab93a26bb39d868a4015572af6e3c6dd76ceb9
Morten Lied Johansen <morten.lied.johansen@nav.no>
2020-05-13 13:04:37 +0200

    Convert unittest to pytest

--- testing/connect-test/tests/test_kakfa_connect.py
+++ testing/connect-test/tests/test_kakfa_connect.py
@@ -1,5 +1,4 @@
-from connect_test import __version__
-import unittest
+import pytest
 import time
 
 from connect_test import complex_type_schema_producer, \
@@ -10,40 +9,35 @@ from connect_test import complex_type_schema_producer, \
     nested_complex_type_schema_producer
 
 
-class TestKafkaConnect(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        """ produce() are slow, and unneccassary to be called several times. to avoid calling them for each test
-            use setUpClass()
-        """
-        super(TestKafkaConnect, cls).setUpClass()
+class TestKafkaConnect():
+    @pytest.fixture(scope="class", autouse=True)
+    def produce(self):
         complex_type_schema_producer.produce()
         flat_schema_producer.produce()
         nested_complex_type_schema_producer.produce()
 
     def test_create_flat_schema_connector(self):
-        flat_schema_connector.create() # should not throw exception
+        flat_schema_connector.create()  # should not throw exception
         time.sleep(2)
         connector_status = flat_schema_connector.get_connector_status()
-        self.assertEqual("RUNNING", connector_status["connector"]["state"])
+        assert "RUNNING" == connector_status["connector"]["state"]
         for task in connector_status["tasks"]:
-            self.assertEqual("RUNNING", task["state"])
+            assert "RUNNING" == task["state"]
 
     def test_create_complex_schema_connector(self):
-        complex_schema_connector.create() # should not throw exception
+        complex_schema_connector.create()  # should not throw exception
         time.sleep(2)
         connector_status = complex_schema_connector.get_connector_status()
-        self.assertEqual("RUNNING", connector_status["connector"]["state"])
+        assert "RUNNING" == connector_status["connector"]["state"]
         for task in connector_status["tasks"]:
-            self.assertEqual("RUNNING", task["state"])
+            assert "RUNNING" == task["state"]
 
     def test_nested_create_complex_schema_connector(self):
-        nested_complex_schema_connector.create() # should not throw exception
+        nested_complex_schema_connector.create()  # should not throw exception
         time.sleep(2)
         connector_status = nested_complex_schema_connector.get_connector_status()
-        self.assertEqual("RUNNING", connector_status["connector"]["state"])
+        assert "RUNNING" == connector_status["connector"]["state"]
         for task in connector_status["tasks"]:
-            self.assertEqual("RUNNING", task["state"])
+            assert "RUNNING" == task["state"]
 
 
